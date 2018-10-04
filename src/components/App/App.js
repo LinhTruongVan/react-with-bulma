584cf949dfc18793b42f955f41727e744bb84113
Linh Truong <linh.truong@codelink.io>
2018-10-04 09:15:12 +0700

    Setup Prettier, Eslint, Flow

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
-import styles from './App.module.scss';
+// @flow
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <section className="hero">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">
-            Bulma hero title
-          </h1>
-          <h2 className="subtitle">
-            Bulma hero subtitle
-          </h2>
-        </div>
+const App = () => (
+  <section className="hero">
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title">Bulma hero title</h1>
+        <h2 className="subtitle">Bulma hero subtitle</h2>
       </div>
-    </section>
-    );
-  }
-}
+    </div>
+  </section>
+)
 
-export default App;
+export default App
